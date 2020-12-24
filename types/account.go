5a90c0e9caa9ceeb4cd4190a2a51b457f5fe61f1
yihau.chen <a122092487@gmail.com>
2020-12-24 10:21:57 +0800

    feat: add account type

--- types/account.go
+++ types/account.go
@@ -1,8 +1,24 @@
 package types
 
 import (
+	"crypto/ed25519"
+	"encoding/hex"
+
 	"github.com/portto/solana-go-sdk/common"
 )
+
+type Account struct {
+	PublicKey  common.PublicKey
+	PrivateKey ed25519.PrivateKey
+}
+
+func AccountFromPrivateKeyBytes(privateKey []byte) Account {
+	sk := ed25519.PrivateKey(privateKey)
+	return Account{
+		PublicKey:  common.PublicKeyFromHex(hex.EncodeToString(sk.Public().(ed25519.PublicKey))),
+		PrivateKey: sk,
+	}
+}
 
 type AccountMeta struct {
 	PubKey     common.PublicKey
