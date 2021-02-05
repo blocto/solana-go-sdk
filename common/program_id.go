54aae834b528d687d7cd6c19b3baef47717296af
yihau.chen <a122092487@gmail.com>
2021-02-05 10:01:02 +0800

    add more solana built-in program id

--- common/program_id.go
+++ common/program_id.go
@@ -2,6 +2,11 @@ package common
 
 var (
 	SystemProgramID                    = PublicKeyFromString("11111111111111111111111111111111")
+	ConfigProgramID                    = PublicKeyFromString("Config1111111111111111111111111111111111111")
+	StakeProgramID                     = PublicKeyFromString("Stake11111111111111111111111111111111111111")
+	VoteProgramID                      = PublicKeyFromString("Vote111111111111111111111111111111111111111")
+	BPFLoaderProgramID                 = PublicKeyFromString("BPFLoader1111111111111111111111111111111111")
+	Secp256k1ProgramID                 = PublicKeyFromString("KeccakSecp256k11111111111111111111111111111")
 	TokenProgramID                     = PublicKeyFromString("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA")
 	SPLAssociatedTokenAccountProgramID = PublicKeyFromString("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL")
 )
