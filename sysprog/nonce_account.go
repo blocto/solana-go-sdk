705257b8fe95f7274f262410caf362e246683bcc
yihau.chen <a122092487@gmail.com>
2021-02-17 11:10:44 +0800

    add create nonce account method

--- sysprog/nonce_account.go
+++ sysprog/nonce_account.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/portto/solana-go-sdk/common"
+	"github.com/portto/solana-go-sdk/types"
 )
 
 const NonceAccountSize = 80
@@ -36,4 +37,37 @@ func NonceAccountDeserialize(data []byte) (NonceAccount, error) {
 		Nonce:            nonce,
 		FeeCalculator:    feeCalculator,
 	}, nil
+}
+
+type CreateNonceAccountParam struct {
+	FromPubkey       common.PublicKey
+	NoncePubkey      common.PublicKey
+	AuthorizedPubkey common.PublicKey
+	Lamports         uint64
+}
+
+func CreateNonceAccountInstruction(param CreateNonceAccountParam) []types.Instruction {
+	return []types.Instruction{
+		CreateAccount(param.FromPubkey, param.NoncePubkey, common.SystemProgramID, param.Lamports, NonceAccountSize),
+		InitializeNonce(param.NoncePubkey, param.AuthorizedPubkey),
+	}
+}
+
+func InitializeNonce(noncePubkey, authPubkey common.PublicKey) types.Instruction {
+	instruction := make([]byte, 4)
+	binary.LittleEndian.PutUint32(instruction, 6)
+
+	data := make([]byte, 0, 36)
+	data = append(data, instruction...)
+	data = append(data, authPubkey.Bytes()...)
+
+	return types.Instruction{
+		Accounts: []types.AccountMeta{
+			{PubKey: noncePubkey, IsSigner: false, IsWritable: true},
+			{PubKey: common.SysVarRecentBlockhashsPubkey, IsSigner: false, IsWritable: false},
+			{PubKey: common.SysVarRentPubkey, IsSigner: false, IsWritable: false},
+		},
+		ProgramID: common.SystemProgramID,
+		Data:      data,
+	}
 }
