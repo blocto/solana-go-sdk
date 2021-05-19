6f8db42bfb796a49f033aa78c66f54e7cb652b0e
Yihau Chen <a122092487@gmail.com>
2021-05-19 20:35:11 +0800

    rpc: add get block time

---
+++ client/get_block_time.go
@@ -0,0 +1,21 @@
+package client
+
+import (
+	"context"
+	"errors"
+)
+
+func (s *Client) GetBlockTime(ctx context.Context, slot uint64) (int64, error) {
+	res := struct {
+		GeneralResponse
+		Result int64 `json:"result"`
+	}{}
+	err := s.request(ctx, "getBlockTime", []interface{}{slot}, &res)
+	if err != nil {
+		return 0, err
+	}
+	if res.Error != (ErrorResponse{}) {
+		return 0, errors.New(res.Error.Message)
+	}
+	return res.Result, nil
+}
