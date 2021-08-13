8ea7f96ff83d961691a7ddb719c8008d8a90643a
yihau <a122092487@gmail.com>
2021-08-13 21:13:54 +0800

    docs: add get version example

---
+++ examples/client/get-version/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/portto/solana-go-sdk/client"
+	"github.com/portto/solana-go-sdk/client/rpc"
+)
+
+func main() {
+	c := client.NewClient(rpc.MainnetRPCEndpoint)
+
+	resp, err := c.GetVersion(context.TODO())
+	if err != nil {
+		log.Fatalf("failed to version info, err: %v", err)
+	}
+
+	fmt.Println("version", resp.SolanaCore)
+}
