eaadb4341bc27953cf1a0bb1bc461d08184c704f
yihau <a122092487@gmail.com>
2021-08-28 23:29:05 +0800

    chore: remove unused curl folder

--- client/curl/request.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ENDPOINT=https://api.devnet.solana.com
-
-request () {
-	echo "==> $1"
-	echo "<== $(curl -s "$ENDPOINT" -X POST -H "Content-Type: application/json" -d "$1")"
-	echo ""
-}
