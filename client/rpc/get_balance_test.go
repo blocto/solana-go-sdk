b05ce64cd3d3f85d834cf6d4322c77b1b3c02242
yihau <a122092487@gmail.com>
2021-08-28 23:03:55 +0800

    refactor: move test common code into test_util

--- client/rpc/get_balance_test.go
+++ client/rpc/get_balance_test.go
@@ -2,12 +2,7 @@ package rpc
 
 import (
 	"context"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestClient_GetBalance(t *testing.T) {
@@ -69,31 +64,4 @@ func TestClient_GetBalance(t *testing.T) {
 			testRpcCall(t, tt)
 		})
 	}
-}
-
-type testRpcCallParam struct {
-	RequestBody      string
-	ResponseBody     string
-	RpcCall          func(RpcClient) (interface{}, error)
-	ExpectedResponse interface{}
-	ExpectedError    error
-}
-
-func testRpcCall(t *testing.T, param testRpcCallParam) {
-	// setup test server
-	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		body, err := ioutil.ReadAll(req.Body)
-		assert.Nil(t, err)
-		assert.JSONEq(t, param.RequestBody, string(body))
-		n, err := rw.Write([]byte(param.ResponseBody))
-		assert.Nil(t, err)
-		assert.Equal(t, len([]byte(param.ResponseBody)), n)
-	}))
-
-	// test call
-	got, err := param.RpcCall(NewRpcClient(server.URL))
-	assert.Equal(t, param.ExpectedError, err)
-	assert.Equal(t, param.ExpectedResponse, got)
-
-	server.Close()
 }
