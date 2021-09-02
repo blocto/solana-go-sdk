61ea6029d7407f46f64fb501a003e6a72f545b3e
yihau <a122092487@gmail.com>
2021-09-03 01:42:45 +0800

    feat(program): add SPL name service state and .sol, twitter handle

---
+++ program/nsprog/state.go
@@ -0,0 +1,26 @@
+package nsprog
+
+import (
+	"fmt"
+
+	"github.com/portto/solana-go-sdk/common"
+)
+
+type NameRecordHeader struct {
+	ParentName common.PublicKey
+	Owner      common.PublicKey
+	Class      common.PublicKey
+	Data       []byte
+}
+
+func NameRecordHeaderFromData(data []byte) (NameRecordHeader, error) {
+	if len(data) < 96 {
+		return NameRecordHeader{}, fmt.Errorf("data length should bigger than 96")
+	}
+	return NameRecordHeader{
+		ParentName: common.PublicKeyFromBytes(data[:32]),
+		Owner:      common.PublicKeyFromBytes(data[32:64]),
+		Class:      common.PublicKeyFromBytes(data[64:96]),
+		Data:       data[96:],
+	}, nil
+}
---
+++ program/nsprog/utils.go
@@ -0,0 +1,39 @@
+package nsprog
+
+import (
+	"crypto/sha256"
+
+	"github.com/portto/solana-go-sdk/common"
+)
+
+var TwitterVerificationAuthority = common.PublicKeyFromString("FvPH7PrVrLGKPfqaf3xJodFTjZriqrAXXLTVWEorTFBi")
+var TwitterRootParentRegisteryKey = common.PublicKeyFromString("4YcexoW3r78zz16J2aqmukBLRwGq6rAvWzJpkYAXqebv")
+var SolTldAuthority = common.PublicKeyFromString("58PwtjSDuFHuUkYjH9BYnnQKHfwo9reZhC2zMJv9JPkx")
+
+const HashPrefix = "SPL Name Service"
+
+// GetHashName ...
+func GetHashName(name string) []byte {
+	h := sha256.Sum256([]byte(HashPrefix + name))
+	return h[:]
+}
+
+// GetNameAccountKey return the pubkey correspond to name
+func GetNameAccountKey(hashName []byte, nameClass, nameParent common.PublicKey) common.PublicKey {
+	seed := [][]byte{
+		hashName,
+		nameClass.Bytes(),
+		nameParent.Bytes(),
+	}
+	pubkey, _, _ := common.FindProgramAddress(seed, common.SPLNameServiceProgramID)
+	return pubkey
+}
+
+// GetTwitterRegistryKey return the pubkey corespond to twitter handle
+func GetTwitterRegistryKey(twitterHandle string) common.PublicKey {
+	return GetNameAccountKey(
+		GetHashName(twitterHandle),
+		common.PublicKey{},
+		TwitterRootParentRegisteryKey,
+	)
+}
