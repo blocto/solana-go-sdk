d156efe47ad2297e65ecac0d1716cadde32d16a4
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2021-10-05 13:58:07 +0300

    fix payer writable

--- program/metaplex/tokenmeta/instruction.go
+++ program/metaplex/tokenmeta/instruction.go
@@ -48,7 +48,7 @@ func CreateMetadataAccount(metadata, mint, mintAuthority, payer, updateAuthority
 			{
 				PubKey:     payer,
 				IsSigner:   true,
-				IsWritable: false,
+				IsWritable: true,
 			},
 			{
 				PubKey:     updateAuthority,
