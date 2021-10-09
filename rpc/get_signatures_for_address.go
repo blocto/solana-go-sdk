0fa7645d8e858fa4c2db8f703c260299ebf8bae3
yihau <a122092487@gmail.com>
2021-10-09 10:19:33 +0800

    feat(rpc): refactor get signatures for address

--- rpc/get_signatures_for_address.go
+++ rpc/get_signatures_for_address.go
@@ -2,10 +2,16 @@ package rpc
 
 import (
 	"context"
-	"errors"
 )
 
-type GetSignaturesForAddress struct {
+// GetSignaturesForAddressResponse is full `getSignaturesForAddress` raw response
+type GetSignaturesForAddressResponse struct {
+	GeneralResponse
+	Result []GetSignaturesForAddressResult `json:"result"`
+}
+
+// GetSignaturesForAddressResult is a part of `getSignaturesForAddress` raw response
+type GetSignaturesForAddressResult struct {
 	Signature string      `json:"signature"`
 	Slot      uint64      `json:"slot"`
 	BlockTime *int64      `json:"blockTime"`
@@ -13,6 +19,7 @@ type GetSignaturesForAddress struct {
 	Memo      *string     `json:"memo"`
 }
 
+// GetSignaturesForAddressConfig is option config of `getSignaturesForAddress`
 type GetSignaturesForAddressConfig struct {
 	Limit      int        `json:"limit,omitempty"` // between 1 and 1000, default: 1000
 	Before     string     `json:"before,omitempty"`
@@ -21,19 +28,20 @@ type GetSignaturesForAddressConfig struct {
 }
 
 // NEW: This method is only available in solana-core v1.7 or newer. Please use "getConfirmedSignaturesForAddress2" for solana-core v1.6
-// GetSignaturesForAddress Returns confirmed signatures for transactions involving an address backwards
+// GetSignaturesForAddress returns confirmed signatures for transactions involving an address backwards
 // in time from the provided signature or most recent confirmed block
-func (s *RpcClient) GetSignaturesForAddress(ctx context.Context, base58Addr string, config GetConfirmedSignaturesForAddressConfig) ([]GetConfirmedSignaturesForAddress, error) {
-	res := struct {
-		GeneralResponse
-		Result []GetConfirmedSignaturesForAddress `json:"result"`
-	}{}
-	err := s.request(ctx, "getSignaturesForAddress", []interface{}{base58Addr, config}, &res)
-	if err != nil {
-		return []GetConfirmedSignaturesForAddress{}, err
-	}
-	if res.Error != nil {
-		return []GetConfirmedSignaturesForAddress{}, errors.New(res.Error.Message)
-	}
-	return res.Result, nil
+func (c *RpcClient) GetSignaturesForAddress(ctx context.Context, base58Addr string) (GetSignaturesForAddressResponse, error) {
+	return c.processGetSignaturesForAddress(c.Call(ctx, "getSignaturesForAddress", base58Addr))
+}
+
+// NEW: This method is only available in solana-core v1.7 or newer. Please use "getConfirmedSignaturesForAddress2" for solana-core v1.6
+// GetSignaturesForAddressWithConfig returns confirmed signatures for transactions involving an address backwards
+// in time from the provided signature or most recent confirmed block
+func (c *RpcClient) GetSignaturesForAddressWithConfig(ctx context.Context, base58Addr string, cfg GetSignaturesForAddressConfig) (GetSignaturesForAddressResponse, error) {
+	return c.processGetSignaturesForAddress(c.Call(ctx, "getSignaturesForAddress", base58Addr, cfg))
+}
+
+func (c *RpcClient) processGetSignaturesForAddress(body []byte, rpcErr error) (res GetSignaturesForAddressResponse, err error) {
+	err = c.processRpcCall(body, rpcErr, &res)
+	return
 }
---
+++ rpc/get_signatures_for_address_test.go
@@ -0,0 +1,187 @@
+package rpc
+
+import (
+	"context"
+	"testing"
+
+	"github.com/portto/solana-go-sdk/pkg/pointer"
+)
+
+func TestGetSignaturesForAddress(t *testing.T) {
+	tests := []testRpcCallParam{
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getSignaturesForAddress", "params":["27kVX7JpPZ1bsrSckbR76mV6GeRqtrjoddubfg2zBpHZ"]}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":[{"blockTime":1633580920,"confirmationStatus":"finalized","err":null,"memo":null,"signature":"26UNKcerqcmHhRgFqtvtWUJZod91dGpkkAtRzKewvsZSoah33VZiFgKPmnHmMhBwsHT4bQjKdCgH88Faex5WkChh","slot":63372},{"blockTime":1633580919,"confirmationStatus":"finalized","err":null,"memo":null,"signature":"vB73C8nWXv2ZwLjCRhTQXnkqqKDafE9uWyycQqajFzQcNFzjRUYq8ZqmtCm4qnwpGxXJWbkkRuyhiQ26zEpfk28","slot":63370},{"blockTime":1633580918,"confirmationStatus":"finalized","err":null,"memo":null,"signature":"4ZAtdQ7wF8EPXVf43ZpyqMiKqpaiGppysSxQ54X31Q92ht1pKgQymiJwRnUc3h4cUjRJfFrVprNJhW6UfybJMAQP","slot":63369},{"blockTime":1633580916,"confirmationStatus":"finalized","err":null,"memo":null,"signature":"63BWysCcY6CcVT9G4FrfD1XuZ2DUswSTuJM5jeULUWwgp3BJhMnAStH3gTkUPXeUEjYoQhVNfd61RxxxWwsezi8y","slot":63367},{"blockTime":1633580915,"confirmationStatus":"finalized","err":null,"memo":null,"signature":"pxS5UZhzvk8p5qiKonkAeVBjP1ipujERPMMNZ2ZexK1PyU8RvZxQwYjiv9YJYP4CpRrHAxTET2rNZ6LNf4aYvDN","slot":63365}],"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetSignaturesForAddress(
+					context.Background(),
+					"27kVX7JpPZ1bsrSckbR76mV6GeRqtrjoddubfg2zBpHZ",
+				)
+			},
+			ExpectedResponse: GetSignaturesForAddressResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: []GetSignaturesForAddressResult{
+					{
+						Signature: "26UNKcerqcmHhRgFqtvtWUJZod91dGpkkAtRzKewvsZSoah33VZiFgKPmnHmMhBwsHT4bQjKdCgH88Faex5WkChh",
+						Slot:      63372,
+						BlockTime: pointer.Int64(1633580920),
+					},
+					{
+						Signature: "vB73C8nWXv2ZwLjCRhTQXnkqqKDafE9uWyycQqajFzQcNFzjRUYq8ZqmtCm4qnwpGxXJWbkkRuyhiQ26zEpfk28",
+						Slot:      63370,
+						BlockTime: pointer.Int64(1633580919),
+					},
+					{
+						Signature: "4ZAtdQ7wF8EPXVf43ZpyqMiKqpaiGppysSxQ54X31Q92ht1pKgQymiJwRnUc3h4cUjRJfFrVprNJhW6UfybJMAQP",
+						Slot:      63369,
+						BlockTime: pointer.Int64(1633580918),
+					},
+					{
+						Signature: "63BWysCcY6CcVT9G4FrfD1XuZ2DUswSTuJM5jeULUWwgp3BJhMnAStH3gTkUPXeUEjYoQhVNfd61RxxxWwsezi8y",
+						Slot:      63367,
+						BlockTime: pointer.Int64(1633580916),
+					},
+					{
+						Signature: "pxS5UZhzvk8p5qiKonkAeVBjP1ipujERPMMNZ2ZexK1PyU8RvZxQwYjiv9YJYP4CpRrHAxTET2rNZ6LNf4aYvDN",
+						Slot:      63365,
+						BlockTime: pointer.Int64(1633580915),
+					},
+				},
+			},
+			ExpectedError: nil,
+		},
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getSignaturesForAddress", "params":["27kVX7JpPZ1bsrSckbR76mV6GeRqtrjoddubfg2zBpHZ", {"limit": 1, "commitment": "confirmed"}]}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":[{"blockTime":1633581563,"confirmationStatus":"finalized","err":null,"memo":null,"signature":"66z7UgyzEozBQ1moxc2ThzGtzbwETZ9bR5ExSUubWhQzqGuX1hQgCGSV1n22o96yuDCFY2dHeMNNLDnf6zjewy7C","slot":64265}],"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetSignaturesForAddressWithConfig(
+					context.Background(),
+					"27kVX7JpPZ1bsrSckbR76mV6GeRqtrjoddubfg2zBpHZ",
+					GetSignaturesForAddressConfig{
+						Limit:      1,
+						Commitment: CommitmentConfirmed,
+					},
+				)
+			},
+			ExpectedResponse: GetSignaturesForAddressResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: []GetSignaturesForAddressResult{
+					{
+						Signature: "66z7UgyzEozBQ1moxc2ThzGtzbwETZ9bR5ExSUubWhQzqGuX1hQgCGSV1n22o96yuDCFY2dHeMNNLDnf6zjewy7C",
+						Slot:      64265,
+						BlockTime: pointer.Int64(1633581563),
+					},
+				},
+			},
+			ExpectedError: nil,
+		},
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getSignaturesForAddress", "params":["27kVX7JpPZ1bsrSckbR76mV6GeRqtrjoddubfg2zBpHZ", {"until": "vB73C8nWXv2ZwLjCRhTQXnkqqKDafE9uWyycQqajFzQcNFzjRUYq8ZqmtCm4qnwpGxXJWbkkRuyhiQ26zEpfk28"}]}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":[{"blockTime":1633581563,"confirmationStatus":"finalized","err":null,"memo":null,"signature":"66z7UgyzEozBQ1moxc2ThzGtzbwETZ9bR5ExSUubWhQzqGuX1hQgCGSV1n22o96yuDCFY2dHeMNNLDnf6zjewy7C","slot":64265},{"blockTime":1633580920,"confirmationStatus":"finalized","err":null,"memo":null,"signature":"26UNKcerqcmHhRgFqtvtWUJZod91dGpkkAtRzKewvsZSoah33VZiFgKPmnHmMhBwsHT4bQjKdCgH88Faex5WkChh","slot":63372}],"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetSignaturesForAddressWithConfig(
+					context.Background(),
+					"27kVX7JpPZ1bsrSckbR76mV6GeRqtrjoddubfg2zBpHZ",
+					GetSignaturesForAddressConfig{
+						Until: "vB73C8nWXv2ZwLjCRhTQXnkqqKDafE9uWyycQqajFzQcNFzjRUYq8ZqmtCm4qnwpGxXJWbkkRuyhiQ26zEpfk28",
+					},
+				)
+			},
+			ExpectedResponse: GetSignaturesForAddressResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: []GetSignaturesForAddressResult{
+					{
+						Signature: "66z7UgyzEozBQ1moxc2ThzGtzbwETZ9bR5ExSUubWhQzqGuX1hQgCGSV1n22o96yuDCFY2dHeMNNLDnf6zjewy7C",
+						Slot:      64265,
+						BlockTime: pointer.Int64(1633581563),
+					},
+					{
+						Signature: "26UNKcerqcmHhRgFqtvtWUJZod91dGpkkAtRzKewvsZSoah33VZiFgKPmnHmMhBwsHT4bQjKdCgH88Faex5WkChh",
+						Slot:      63372,
+						BlockTime: pointer.Int64(1633580920),
+					},
+				},
+			},
+			ExpectedError: nil,
+		},
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getSignaturesForAddress", "params":["27kVX7JpPZ1bsrSckbR76mV6GeRqtrjoddubfg2zBpHZ", {"before": "4ZAtdQ7wF8EPXVf43ZpyqMiKqpaiGppysSxQ54X31Q92ht1pKgQymiJwRnUc3h4cUjRJfFrVprNJhW6UfybJMAQP"}]}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":[{"blockTime":1633580916,"confirmationStatus":"finalized","err":null,"memo":null,"signature":"63BWysCcY6CcVT9G4FrfD1XuZ2DUswSTuJM5jeULUWwgp3BJhMnAStH3gTkUPXeUEjYoQhVNfd61RxxxWwsezi8y","slot":63367},{"blockTime":1633580915,"confirmationStatus":"finalized","err":null,"memo":null,"signature":"pxS5UZhzvk8p5qiKonkAeVBjP1ipujERPMMNZ2ZexK1PyU8RvZxQwYjiv9YJYP4CpRrHAxTET2rNZ6LNf4aYvDN","slot":63365}],"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetSignaturesForAddressWithConfig(
+					context.Background(),
+					"27kVX7JpPZ1bsrSckbR76mV6GeRqtrjoddubfg2zBpHZ",
+					GetSignaturesForAddressConfig{
+						Before: "4ZAtdQ7wF8EPXVf43ZpyqMiKqpaiGppysSxQ54X31Q92ht1pKgQymiJwRnUc3h4cUjRJfFrVprNJhW6UfybJMAQP",
+					},
+				)
+			},
+			ExpectedResponse: GetSignaturesForAddressResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: []GetSignaturesForAddressResult{
+					{
+						Signature: "63BWysCcY6CcVT9G4FrfD1XuZ2DUswSTuJM5jeULUWwgp3BJhMnAStH3gTkUPXeUEjYoQhVNfd61RxxxWwsezi8y",
+						Slot:      63367,
+						BlockTime: pointer.Int64(1633580916),
+					},
+					{
+						Signature: "pxS5UZhzvk8p5qiKonkAeVBjP1ipujERPMMNZ2ZexK1PyU8RvZxQwYjiv9YJYP4CpRrHAxTET2rNZ6LNf4aYvDN",
+						Slot:      63365,
+						BlockTime: pointer.Int64(1633580915),
+					},
+				},
+			},
+			ExpectedError: nil,
+		},
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getSignaturesForAddress", "params":["27kVX7JpPZ1bsrSckbR76mV6GeRqtrjoddubfg2zBpHZ", {"limit": 1, "before": "4ZAtdQ7wF8EPXVf43ZpyqMiKqpaiGppysSxQ54X31Q92ht1pKgQymiJwRnUc3h4cUjRJfFrVprNJhW6UfybJMAQP"}]}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":[{"blockTime":1633580916,"confirmationStatus":"finalized","err":null,"memo":null,"signature":"63BWysCcY6CcVT9G4FrfD1XuZ2DUswSTuJM5jeULUWwgp3BJhMnAStH3gTkUPXeUEjYoQhVNfd61RxxxWwsezi8y","slot":63367}],"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetSignaturesForAddressWithConfig(
+					context.Background(),
+					"27kVX7JpPZ1bsrSckbR76mV6GeRqtrjoddubfg2zBpHZ",
+					GetSignaturesForAddressConfig{
+						Limit:  1,
+						Before: "4ZAtdQ7wF8EPXVf43ZpyqMiKqpaiGppysSxQ54X31Q92ht1pKgQymiJwRnUc3h4cUjRJfFrVprNJhW6UfybJMAQP",
+					},
+				)
+			},
+			ExpectedResponse: GetSignaturesForAddressResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: []GetSignaturesForAddressResult{
+					{
+						Signature: "63BWysCcY6CcVT9G4FrfD1XuZ2DUswSTuJM5jeULUWwgp3BJhMnAStH3gTkUPXeUEjYoQhVNfd61RxxxWwsezi8y",
+						Slot:      63367,
+						BlockTime: pointer.Int64(1633580916),
+					},
+				},
+			},
+			ExpectedError: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			testRpcCall(t, tt)
+		})
+	}
+}
