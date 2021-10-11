e7c344e68dd3499a1f795eae52281ce52029ed5a
yihau <a122092487@gmail.com>
2021-10-12 02:03:56 +0800

    feat(rpc): refactor get epoch info

--- rpc/get_epoch_info.go
+++ rpc/get_epoch_info.go
@@ -2,23 +2,37 @@ package rpc
 
 import "context"
 
+// GetEpochInfoResponse is a full raw rpc response of `getEpochInfo`
 type GetEpochInfoResponse struct {
-	AbsoluteSlot uint64 `json:"absoluteSlot"`
-	BlockHeight  uint64 `json:"blockHeight"`
-	Epoch        uint64 `json:"epoch"`
-	SlotIndex    uint64 `json:"slotIndex"`
-	SlotsInEpoch uint64 `json:"slotsInEpoch"`
+	GeneralResponse
+	Result GetEpochInfoResponseResult `json:"result"`
 }
 
-// GetEpochInfo returns information about the current epoch
-func (s *RpcClient) GetEpochInfo(ctx context.Context, commitment Commitment) (GetEpochInfoResponse, error) {
-	res := struct {
-		GeneralResponse
-		Result GetEpochInfoResponse `json:"result"`
-	}{}
-	err := s.request(ctx, "getEpochInfo", []interface{}{map[string]interface{}{"commitment": commitment}}, &res)
-	if err != nil {
-		return GetEpochInfoResponse{}, err
-	}
-	return res.Result, nil
+type GetEpochInfoResponseResult struct {
+	AbsoluteSlot     uint64  `json:"absoluteSlot"`
+	BlockHeight      uint64  `json:"blockHeight"`
+	Epoch            uint64  `json:"epoch"`
+	SlotIndex        uint64  `json:"slotIndex"`
+	SlotsInEpoch     uint64  `json:"slotsInEpoch"`
+	TransactionCount *uint64 `json:"transactionCount"`
+}
+
+// GetEpochInfoConfig is a option config for `getEpochInfo`
+type GetEpochInfoConfig struct {
+	Commitment Commitment `json:"commitment,omitempty"`
+}
+
+// GetEpochInfo returns the SOL balance
+func (c *RpcClient) GetEpochInfo(ctx context.Context) (GetEpochInfoResponse, error) {
+	return c.processGetEpochInfo(c.Call(ctx, "getEpochInfo"))
+}
+
+// GetEpochInfoWithConfig returns the SOL balance
+func (c *RpcClient) GetEpochInfoWithConfig(ctx context.Context, cfg GetEpochInfoConfig) (GetEpochInfoResponse, error) {
+	return c.processGetEpochInfo(c.Call(ctx, "getEpochInfo", cfg))
+}
+
+func (c *RpcClient) processGetEpochInfo(body []byte, rpcErr error) (res GetEpochInfoResponse, err error) {
+	err = c.processRpcCall(body, rpcErr, &res)
+	return
 }
---
+++ rpc/get_epoch_info_test.go
@@ -0,0 +1,71 @@
+package rpc
+
+import (
+	"context"
+	"testing"
+
+	"github.com/portto/solana-go-sdk/pkg/pointer"
+)
+
+func TestGetEpochInfo(t *testing.T) {
+	tests := []testRpcCallParam{
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getEpochInfo"}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":{"absoluteSlot":86715160,"blockHeight":84901536,"epoch":200,"slotIndex":315160,"slotsInEpoch":432000,"transactionCount":2265984079},"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetEpochInfo(
+					context.TODO(),
+				)
+			},
+			ExpectedResponse: GetEpochInfoResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: GetEpochInfoResponseResult{
+					AbsoluteSlot:     86715160,
+					BlockHeight:      84901536,
+					Epoch:            200,
+					SlotIndex:        315160,
+					SlotsInEpoch:     432000,
+					TransactionCount: pointer.Uint64(2265984079),
+				},
+			},
+			ExpectedError: nil,
+		},
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getEpochInfo", "params":[{"commitment": "processed"}]}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":{"absoluteSlot":86715194,"blockHeight":84901570,"epoch":200,"slotIndex":315194,"slotsInEpoch":432000,"transactionCount":2265987458},"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetEpochInfoWithConfig(
+					context.TODO(),
+					GetEpochInfoConfig{
+						Commitment: CommitmentProcessed,
+					},
+				)
+			},
+			ExpectedResponse: GetEpochInfoResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: GetEpochInfoResponseResult{
+					AbsoluteSlot:     86715194,
+					BlockHeight:      84901570,
+					Epoch:            200,
+					SlotIndex:        315194,
+					SlotsInEpoch:     432000,
+					TransactionCount: pointer.Uint64(2265987458),
+				},
+			},
+			ExpectedError: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			testRpcCall(t, tt)
+		})
+	}
+}
