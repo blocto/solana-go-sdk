2024217a40fb6059b944da03836f0a613a0d0ba3
yihau <a122092487@gmail.com>
2021-10-11 22:48:17 +0800

    feat(rpc): refoctor get blocks

--- rpc/get_blocks.go
+++ rpc/get_blocks.go
@@ -2,21 +2,32 @@ package rpc
 
 import "context"
 
+// GetBlocksResponse is a full raw rpc response of `getBlocks`
+type GetBlocksResponse struct {
+	GeneralResponse
+	Result []uint64 `json:"result"`
+}
+
+// GetBlocksConfig is a option config for `getBlocks`
 type GetBlocksConfig struct {
 	Commitment Commitment `json:"commitment,omitempty"`
 }
 
 // NEW: This method is only available in solana-core v1.7 or newer. Please use getConfirmedBlocks for solana-core v1.6
 // GetBlocks returns a list of confirmed blocks between two slots
-// Max range allowed is 500,000 slots
-func (s *RpcClient) GetBlocks(ctx context.Context, startSlot uint64, endSlot uint64, cfg GetBlocksConfig) ([]uint64, error) {
-	res := struct {
-		GeneralResponse
-		Result []uint64 `json:"result"`
-	}{}
-	err := s.request(ctx, "getBlocks", []interface{}{startSlot, endSlot, cfg}, &res)
-	if err != nil {
-		return nil, err
-	}
-	return res.Result, nil
+// Max range allowed is 500,000 slot
+func (c *RpcClient) GetBlocks(ctx context.Context, startSlot uint64, endSlot uint64) (GetBlocksResponse, error) {
+	return c.processGetBlocks(c.Call(ctx, "getBlocks", startSlot, endSlot))
+}
+
+// NEW: This method is only available in solana-core v1.7 or newer. Please use getConfirmedBlocks for solana-core v1.6
+// GetBlocks returns a list of confirmed blocks between two slots
+// Max range allowed is 500,000 slot
+func (c *RpcClient) GetBlocksWithConfig(ctx context.Context, startSlot uint64, endSlot uint64, cfg GetBlocksConfig) (GetBlocksResponse, error) {
+	return c.processGetBlocks(c.Call(ctx, "getBlocks", startSlot, endSlot, cfg))
+}
+
+func (c *RpcClient) processGetBlocks(body []byte, rpcErr error) (res GetBlocksResponse, err error) {
+	err = c.processRpcCall(body, rpcErr, &res)
+	return
 }
---
+++ rpc/get_blocks_test.go
@@ -0,0 +1,59 @@
+package rpc
+
+import (
+	"context"
+	"testing"
+)
+
+func TestGetBlocks(t *testing.T) {
+	tests := []testRpcCallParam{
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getBlocks", "params":[86686567, 86686578]}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":[86686567,86686572,86686573,86686574,86686575,86686576,86686577,86686578],"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetBlocks(
+					context.TODO(),
+					86686567,
+					86686578,
+				)
+			},
+			ExpectedResponse: GetBlocksResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: []uint64{86686567, 86686572, 86686573, 86686574, 86686575, 86686576, 86686577, 86686578},
+			},
+			ExpectedError: nil,
+		},
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getBlocks", "params":[86686567, 86686578, {"commitment": "confirmed"}]}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":[86686567,86686572,86686573,86686574,86686575,86686576,86686577,86686578],"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetBlocksWithConfig(
+					context.TODO(),
+					86686567,
+					86686578,
+					GetBlocksConfig{
+						Commitment: CommitmentConfirmed,
+					},
+				)
+			},
+			ExpectedResponse: GetBlocksResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: []uint64{86686567, 86686572, 86686573, 86686574, 86686575, 86686576, 86686577, 86686578},
+			},
+			ExpectedError: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			testRpcCall(t, tt)
+		})
+	}
+}
