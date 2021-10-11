89bc572c6d48419e204e4f89cfb659b7a818b6a3
yihau <a122092487@gmail.com>
2021-10-11 23:00:02 +0800

    feat(rpc): refactor get blocks with limit

--- rpc/get_blocks_with_limit.go
+++ rpc/get_blocks_with_limit.go
@@ -2,20 +2,30 @@ package rpc
 
 import "context"
 
+// GetBlocksWithLimitResponse is a full raw rpc response of `getBlocksWithLimit`
+type GetBlocksWithLimitResponse struct {
+	GeneralResponse
+	Result []uint64 `json:"result"`
+}
+
+// GetBlocksWithLimitConfig is a option config for `getBlocksWithLimit`
 type GetBlocksWithLimitConfig struct {
 	Commitment Commitment `json:"commitment,omitempty"`
 }
 
-// NEW: This method is only available in solana-core v1.7 or newer. Please use getConfirmedBlocksWithLimit for solana-core v1.6
-// GetBlocksWithLimit returns a list of confirmed blocks starting at the given slot
-func (s *RpcClient) GetBlocksWithLimit(ctx context.Context, startSlot uint64, limit uint64, cfg GetBlocksWithLimitConfig) ([]uint64, error) {
-	res := struct {
-		GeneralResponse
-		Result []uint64 `json:"result"`
-	}{}
-	err := s.request(ctx, "getBlocksWithLimit", []interface{}{startSlot, limit, cfg}, &res)
-	if err != nil {
-		return nil, err
-	}
-	return res.Result, nil
+// NEW: This method is only available in solana-core v1.7 or newer.
+// GetBlocksWithLimit eturns a list of confirmed blocks starting at the given slot
+func (c *RpcClient) GetBlocksWithLimit(ctx context.Context, startSlot uint64, limit uint64) (GetBlocksWithLimitResponse, error) {
+	return c.processGetBlocksWithLimit(c.Call(ctx, "getBlocksWithLimit", startSlot, limit))
+}
+
+// NEW: This method is only available in solana-core v1.7 or newer.
+// GetBlocksWithLimit eturns a list of confirmed blocks starting at the given slot
+func (c *RpcClient) GetBlocksWithLimitWithConfig(ctx context.Context, startSlot uint64, limit uint64, cfg GetBlocksWithLimitConfig) (GetBlocksWithLimitResponse, error) {
+	return c.processGetBlocksWithLimit(c.Call(ctx, "getBlocksWithLimit", startSlot, limit, cfg))
+}
+
+func (c *RpcClient) processGetBlocksWithLimit(body []byte, rpcErr error) (res GetBlocksWithLimitResponse, err error) {
+	err = c.processRpcCall(body, rpcErr, &res)
+	return
 }
---
+++ rpc/get_blocks_with_limit_test.go
@@ -0,0 +1,59 @@
+package rpc
+
+import (
+	"context"
+	"testing"
+)
+
+func TestGetBlocksWithLimit(t *testing.T) {
+	tests := []testRpcCallParam{
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getBlocksWithLimit", "params":[86686567, 15]}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":[86686567,86686572,86686573,86686574,86686575,86686576,86686577,86686578,86686579,86686580,86686581,86686582,86686583,86686584,86686585],"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetBlocksWithLimit(
+					context.TODO(),
+					86686567,
+					15,
+				)
+			},
+			ExpectedResponse: GetBlocksWithLimitResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: []uint64{86686567, 86686572, 86686573, 86686574, 86686575, 86686576, 86686577, 86686578, 86686579, 86686580, 86686581, 86686582, 86686583, 86686584, 86686585},
+			},
+			ExpectedError: nil,
+		},
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getBlocksWithLimit", "params":[86686567, 15, {"commitment": "confirmed"}]}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":[86686567,86686572,86686573,86686574,86686575,86686576,86686577,86686578,86686579,86686580,86686581,86686582,86686583,86686584,86686585],"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetBlocksWithLimitWithConfig(
+					context.TODO(),
+					86686567,
+					15,
+					GetBlocksWithLimitConfig{
+						Commitment: CommitmentConfirmed,
+					},
+				)
+			},
+			ExpectedResponse: GetBlocksWithLimitResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: []uint64{86686567, 86686572, 86686573, 86686574, 86686575, 86686576, 86686577, 86686578, 86686579, 86686580, 86686581, 86686582, 86686583, 86686584, 86686585},
+			},
+			ExpectedError: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			testRpcCall(t, tt)
+		})
+	}
+}
