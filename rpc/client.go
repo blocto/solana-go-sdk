60252b480aa1b4bbd110e406b8dabd4bc04f2f10
yihau <a122092487@gmail.com>
2021-10-15 00:53:52 +0800

    feat(rpc): remove some old function

--- rpc/client.go
+++ rpc/client.go
@@ -108,52 +108,6 @@ func preparePayload(params []interface{}) ([]byte, error) {
 	return j, nil
 }
 
-// TODO use Call
-func (s *RpcClient) request(ctx context.Context, method string, params []interface{}, response interface{}) error {
-	// post data
-	j, err := json.Marshal(map[string]interface{}{
-		"jsonrpc": "2.0",
-		"id":      0,
-		"method":  method,
-		"params":  params,
-	})
-	if err != nil {
-		return err
-	}
-
-	// post request
-	req, err := http.NewRequestWithContext(ctx, "POST", s.endpoint, bytes.NewBuffer(j))
-	if err != nil {
-		return err
-	}
-	req.Header.Add("Content-Type", "application/json")
-
-	// http client and send request
-	httpclient := &http.Client{}
-	res, err := httpclient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	// parse body
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
-	if len(body) != 0 {
-		if err := json.Unmarshal(body, &response); err != nil {
-			return err
-		}
-	}
-
-	// return result
-	if res.StatusCode < 200 || res.StatusCode > 300 {
-		return fmt.Errorf("get status code: %d", res.StatusCode)
-	}
-	return nil
-}
-
 func (c *RpcClient) processRpcCall(body []byte, rpcErr error, res interface{}) error {
 	if rpcErr != nil {
 		return fmt.Errorf("rpc: call error, err: %v", rpcErr)
