96b05177f0146fa7e7433767992b8230c3720a1e
yihau <a122092487@gmail.com>
2021-10-27 01:30:08 +0800

    feat(rpc): add getFeeRateGovernor

---
+++ rpc/get_fee_rate_governor.go
@@ -0,0 +1,36 @@
+package rpc
+
+import "context"
+
+// GetFeeRateGovernorResponse is a full raw rpc response of `getFeeRateGovernor`
+type GetFeeRateGovernorResponse struct {
+	GeneralResponse
+	Result GetFeeRateGovernorResponseResult `json:"result"`
+}
+
+type GetFeeRateGovernorResponseResult struct {
+	Context Context                               `json:"context"`
+	Value   GetFeeRateGovernorResponseResultValue `json:"value"`
+}
+
+type GetFeeRateGovernorResponseResultValue struct {
+	FeeRateGovernor FeeRateGovernor `json:"feeRateGovernor"`
+}
+
+type FeeRateGovernor struct {
+	MaxLamportsPerSignature    uint64 `json:"maxLamportsPerSignature"`
+	MinLamportsPerSignature    uint64 `json:"minLamportsPerSignature"`
+	TargetLamportsPerSignature uint64 `json:"targetLamportsPerSignature"`
+	TargetSignaturesPerSlot    uint64 `json:"targetSignaturesPerSlot"`
+	BurnPercent                uint8  `json:"burnPercent"`
+}
+
+// GetFeeRateGovernor returns the fee rate governor information from the root bank
+func (c *RpcClient) GetFeeRateGovernor(ctx context.Context) (GetFeeRateGovernorResponse, error) {
+	return c.processGetFeeRateGovernor(c.Call(ctx, "getFeeRateGovernor"))
+}
+
+func (c *RpcClient) processGetFeeRateGovernor(body []byte, rpcErr error) (res GetFeeRateGovernorResponse, err error) {
+	err = c.processRpcCall(body, rpcErr, &res)
+	return
+}
---
+++ rpc/get_fee_rate_governor_test.go
@@ -0,0 +1,47 @@
+package rpc
+
+import (
+	"context"
+	"testing"
+)
+
+func TestGetFeeRateGovernor(t *testing.T) {
+	tests := []testRpcCallParam{
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getFeeRateGovernor"}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":{"context":{"slot":15230},"value":{"feeRateGovernor":{"burnPercent":50,"maxLamportsPerSignature":100000,"minLamportsPerSignature":5000,"targetLamportsPerSignature":10000,"targetSignaturesPerSlot":20000}}},"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetFeeRateGovernor(
+					context.TODO(),
+				)
+			},
+			ExpectedResponse: GetFeeRateGovernorResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: GetFeeRateGovernorResponseResult{
+					Context: Context{
+						Slot: 15230,
+					},
+					Value: GetFeeRateGovernorResponseResultValue{
+						FeeRateGovernor: FeeRateGovernor{
+							MaxLamportsPerSignature:    100000,
+							MinLamportsPerSignature:    5000,
+							TargetLamportsPerSignature: 10000,
+							TargetSignaturesPerSlot:    20000,
+							BurnPercent:                50,
+						},
+					},
+				},
+			},
+			ExpectedError: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			testRpcCall(t, tt)
+		})
+	}
+}
