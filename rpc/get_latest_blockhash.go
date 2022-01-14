130caadae6e77affbdb355b9ee0777753ac5826d
yihau <a122092487@gmail.com>
2022-01-14 13:32:10 +0800

    feat(rpc): add getLatestBlockhash

---
+++ rpc/get_latest_blockhash.go
@@ -0,0 +1,45 @@
+package rpc
+
+import (
+	"context"
+)
+
+// GetLatestBlockhashResponse is a full raw rpc response of `getLatestBlockhash`
+type GetLatestBlockhashResponse struct {
+	GeneralResponse
+	Result GetLatestBlockhashResult `json:"result"`
+}
+
+// GetLatestBlockhashResult is a part of raw rpc response of `getLatestBlockhash`
+type GetLatestBlockhashResult struct {
+	Context Context                 `json:"context"`
+	Value   GetLatestBlockhashValue `json:"value"`
+}
+
+// GetLatestBlockhashResult is a part of raw rpc response of `getLatestBlockhash`
+type GetLatestBlockhashValue struct {
+	Blockhash              string `json:"blockhash"`
+	LatestValidBlockHeight uint64 `json:"lastValidBlockHeight"`
+}
+
+// GetLatestBlockhashConfig is a option config for `getLatestBlockhash`
+type GetLatestBlockhashConfig struct {
+	Commitment Commitment `json:"commitment,omitempty"`
+}
+
+// NEW: This method is only available in solana-core v1.9 or newer. Please use getRecentBlockhash for solana-core v1.8
+// GetLatestBlockhash returns the SOL balance
+func (c *RpcClient) GetLatestBlockhash(ctx context.Context) (GetLatestBlockhashResponse, error) {
+	return c.processGetLatestBlockhash(c.Call(ctx, "getLatestBlockhash"))
+}
+
+// NEW: This method is only available in solana-core v1.9 or newer. Please use getRecentBlockhash for solana-core v1.8
+// GetLatestBlockhashWithConfig returns the SOL balance
+func (c *RpcClient) GetLatestBlockhashWithConfig(ctx context.Context, cfg GetLatestBlockhashConfig) (GetLatestBlockhashResponse, error) {
+	return c.processGetLatestBlockhash(c.Call(ctx, "getLatestBlockhash", cfg))
+}
+
+func (c *RpcClient) processGetLatestBlockhash(body []byte, rpcErr error) (res GetLatestBlockhashResponse, err error) {
+	err = c.processRpcCall(body, rpcErr, &res)
+	return
+}
---
+++ rpc/get_latest_blockhash_test.go
@@ -0,0 +1,112 @@
+package rpc
+
+import (
+	"context"
+	"testing"
+)
+
+func TestGetLatestBlockhash(t *testing.T) {
+	tests := []testRpcCallParam{
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getLatestBlockhash"}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":{"context":{"slot":112872139},"value":{"blockhash":"9K9GnvWXn9zYitQdHUSYzvjLjebnviwEFaWgWqHDU3ve","lastValidBlockHeight":92248597}},"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetLatestBlockhash(context.TODO())
+			},
+			ExpectedResponse: GetLatestBlockhashResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: GetLatestBlockhashResult{
+					Context: Context{
+						Slot: 112872139,
+					},
+					Value: GetLatestBlockhashValue{
+						Blockhash:              "9K9GnvWXn9zYitQdHUSYzvjLjebnviwEFaWgWqHDU3ve",
+						LatestValidBlockHeight: 92248597,
+					},
+				},
+			},
+			ExpectedError: nil,
+		},
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getLatestBlockhash", "params":[{"commitment": "processed"}]}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":{"context":{"slot":112871314},"value":{"blockhash":"3H2pwJD6pTrEveh5xcwHXToLn7txt5uTW6CPzCan4ZKL","lastValidBlockHeight":92247902}},"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetLatestBlockhashWithConfig(context.TODO(), GetLatestBlockhashConfig{Commitment: CommitmentProcessed})
+			},
+			ExpectedResponse: GetLatestBlockhashResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: GetLatestBlockhashResult{
+					Context: Context{
+						Slot: 112871314,
+					},
+					Value: GetLatestBlockhashValue{
+						Blockhash:              "3H2pwJD6pTrEveh5xcwHXToLn7txt5uTW6CPzCan4ZKL",
+						LatestValidBlockHeight: 92247902,
+					},
+				},
+			},
+			ExpectedError: nil,
+		},
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getLatestBlockhash", "params":[{"commitment": "confirmed"}]}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":{"context":{"slot":112871311},"value":{"blockhash":"FXuaK93DmxWt98bv3wYMdE3TMnY2o8e3h85KrGWEUAzv","lastValidBlockHeight":92247899}},"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetLatestBlockhashWithConfig(context.TODO(), GetLatestBlockhashConfig{Commitment: CommitmentConfirmed})
+			},
+			ExpectedResponse: GetLatestBlockhashResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: GetLatestBlockhashResult{
+					Context: Context{
+						Slot: 112871311,
+					},
+					Value: GetLatestBlockhashValue{
+						Blockhash:              "FXuaK93DmxWt98bv3wYMdE3TMnY2o8e3h85KrGWEUAzv",
+						LatestValidBlockHeight: 92247899,
+					},
+				},
+			},
+			ExpectedError: nil,
+		},
+		{
+			RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getLatestBlockhash", "params":[{"commitment": "finalized"}]}`,
+			ResponseBody: `{"jsonrpc":"2.0","result":{"context":{"slot":112871221},"value":{"blockhash":"21f41sJRvMV8Tc3R5bTTA3n3yBLuoocSkgb8zj1vmEJa","lastValidBlockHeight":92247838}},"id":1}`,
+			RpcCall: func(rc RpcClient) (interface{}, error) {
+				return rc.GetLatestBlockhashWithConfig(context.TODO(), GetLatestBlockhashConfig{Commitment: CommitmentFinalized})
+			},
+			ExpectedResponse: GetLatestBlockhashResponse{
+				GeneralResponse: GeneralResponse{
+					JsonRPC: "2.0",
+					ID:      1,
+					Error:   nil,
+				},
+				Result: GetLatestBlockhashResult{
+					Context: Context{
+						Slot: 112871221,
+					},
+					Value: GetLatestBlockhashValue{
+						Blockhash:              "21f41sJRvMV8Tc3R5bTTA3n3yBLuoocSkgb8zj1vmEJa",
+						LatestValidBlockHeight: 92247838,
+					},
+				},
+			},
+			ExpectedError: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			testRpcCall(t, tt)
+		})
+	}
+}
