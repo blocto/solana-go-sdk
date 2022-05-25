7ad9b075ec82b74876a082fc914275067045fc97
yihau <a122092487@gmail.com>
2022-05-26 00:07:06 +0800

    feat: add SetComputeUnitLimit and SetComputeUnitPrice

--- program/cmptbdgprog/instruction.go
+++ program/cmptbdgprog/instruction.go
@@ -65,3 +65,52 @@ func RequestHeapFrame(param RequestHeapFrameParam) types.Instruction {
 		Data:      data,
 	}
 }
+
+type SetComputeUnitLimitParam struct {
+	Units uint32
+}
+
+// SetComputeUnitLimit set a specific compute unit limit that the transaction is allowed to consume.
+func SetComputeUnitLimit(param SetComputeUnitLimitParam) types.Instruction {
+	data, err := borsh.Serialize(struct {
+		Instruction Instruction
+		Units       uint32
+	}{
+		Instruction: InstructionSetComputeUnitLimit,
+		Units:       param.Units,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return types.Instruction{
+		ProgramID: common.ComputeBudgetProgramID,
+		Accounts:  []types.AccountMeta{},
+		Data:      data,
+	}
+}
+
+type SetComputeUnitPriceParam struct {
+	MicroLamports uint64
+}
+
+// SetComputeUnitPrice set a compute unit price in "micro-lamports" to pay a higher transaction
+// fee for higher transaction prioritization.
+func SetComputeUnitPrice(param SetComputeUnitPriceParam) types.Instruction {
+	data, err := borsh.Serialize(struct {
+		Instruction   Instruction
+		MicroLamports uint64
+	}{
+		Instruction:   InstructionSetComputeUnitPrice,
+		MicroLamports: param.MicroLamports,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return types.Instruction{
+		ProgramID: common.ComputeBudgetProgramID,
+		Accounts:  []types.AccountMeta{},
+		Data:      data,
+	}
+}
