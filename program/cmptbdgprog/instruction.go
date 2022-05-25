811604355f3e74768c90d251a733516b582bed75
Yihau Chen <a122092487@gmail.com>
2022-05-26 00:21:49 +0800

    feat: add SetComputeUnitLimit and SetComputeUnitPrice to Compute Budget Program (#54)
    * feat: add new instruction type
    
    * feat: add SetComputeUnitLimit and SetComputeUnitPrice
    
    * docs: add set compute uint limit example
    
    * docs: add set compute uint price example

--- program/cmptbdgprog/instruction.go
+++ program/cmptbdgprog/instruction.go
@@ -11,6 +11,8 @@ type Instruction borsh.Enum
 const (
 	InstructionRequestUnits Instruction = iota
 	InstructionRequestHeapFrame
+	InstructionSetComputeUnitLimit
+	InstructionSetComputeUnitPrice
 )
 
 type RequestUnitsParam struct {
@@ -52,6 +54,55 @@ func RequestHeapFrame(param RequestHeapFrameParam) types.Instruction {
 	}{
 		Instruction: InstructionRequestHeapFrame,
 		Bytes:       param.Bytes,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return types.Instruction{
+		ProgramID: common.ComputeBudgetProgramID,
+		Accounts:  []types.AccountMeta{},
+		Data:      data,
+	}
+}
+
+type SetComputeUnitLimitParam struct {
+	Units uint32
+}
+
+// SetComputeUnitLimit set a specific compute unit limit that the transaction is allowed to consume.
+func SetComputeUnitLimit(param SetComputeUnitLimitParam) types.Instruction {
+	data, err := borsh.Serialize(struct {
+		Instruction Instruction
+		Units       uint32
+	}{
+		Instruction: InstructionSetComputeUnitLimit,
+		Units:       param.Units,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return types.Instruction{
+		ProgramID: common.ComputeBudgetProgramID,
+		Accounts:  []types.AccountMeta{},
+		Data:      data,
+	}
+}
+
+type SetComputeUnitPriceParam struct {
+	MicroLamports uint64
+}
+
+// SetComputeUnitPrice set a compute unit price in "micro-lamports" to pay a higher transaction
+// fee for higher transaction prioritization.
+func SetComputeUnitPrice(param SetComputeUnitPriceParam) types.Instruction {
+	data, err := borsh.Serialize(struct {
+		Instruction   Instruction
+		MicroLamports uint64
+	}{
+		Instruction:   InstructionSetComputeUnitPrice,
+		MicroLamports: param.MicroLamports,
 	})
 	if err != nil {
 		panic(err)
--- program/cmptbdgprog/instruction_test.go
+++ program/cmptbdgprog/instruction_test.go
@@ -70,3 +70,65 @@ func TestRequestHeapFrame(t *testing.T) {
 		})
 	}
 }
+
+func TestSetComputeUnitLimit(t *testing.T) {
+	type args struct {
+		param SetComputeUnitLimitParam
+	}
+	tests := []struct {
+		name string
+		args args
+		want types.Instruction
+	}{
+		{
+			args: args{
+				param: SetComputeUnitLimitParam{
+					Units: 1000,
+				},
+			},
+			want: types.Instruction{
+				ProgramID: common.ComputeBudgetProgramID,
+				Accounts:  []types.AccountMeta{},
+				Data:      []byte{2, 232, 3, 0, 0},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SetComputeUnitLimit(tt.args.param); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SetComputeUnitLimit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSetComputeUnitPrice(t *testing.T) {
+	type args struct {
+		param SetComputeUnitPriceParam
+	}
+	tests := []struct {
+		name string
+		args args
+		want types.Instruction
+	}{
+		{
+			args: args{
+				param: SetComputeUnitPriceParam{
+					MicroLamports: 1000,
+				},
+			},
+			want: types.Instruction{
+				ProgramID: common.ComputeBudgetProgramID,
+				Accounts:  []types.AccountMeta{},
+				Data:      []byte{3, 232, 3, 0, 0, 0, 0, 0, 0},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SetComputeUnitPrice(tt.args.param); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SetComputeUnitPrice() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
