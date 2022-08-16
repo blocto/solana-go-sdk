c80a734619bca05580ac8a6175f3ad563c12d9d6
Yihau Chen <a122092487@gmail.com>
2022-08-16 18:19:01 +0800

    add accountEncoding

--- rpc/types.go
+++ rpc/types.go
@@ -13,6 +13,15 @@ type Context struct {
 	Slot uint64 `json:"slot"`
 }
 
+type AccountEncoding string
+
+const (
+	AccountEncodingBase58     AccountEncoding = "base58"
+	AccountEncodingJsonParsed AccountEncoding = "jsonParsed"
+	AccountEncodingBase64     AccountEncoding = "base64"
+	AccountEncodingBase64Zstd AccountEncoding = "base64+zstd"
+)
+
 type AccountInfo struct {
 	Lamports   uint64 `json:"lamports"`
 	Owner      string `json:"owner"`
