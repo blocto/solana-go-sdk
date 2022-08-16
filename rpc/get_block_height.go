46208f002075a6778876d3b1d8b71885f67d36e8
Yihau Chen <a122092487@gmail.com>
2022-08-16 23:52:35 +0800

    get block height use generic response

--- rpc/get_block_height.go
+++ rpc/get_block_height.go
@@ -4,11 +4,7 @@ import (
 	"context"
 )
 
-// GetBlockHeightResponse is a rpc response of `getBlockHeight`
-type GetBlockHeightResponse struct {
-	GeneralResponse
-	Result uint64 `json:"result"`
-}
+type GetBlockHeightResponse JsonRpcResponse[uint64]
 
 // GetBlockHeightConfig is a option config for `getBlockHeight`
 type GetBlockHeightConfig struct {
@@ -16,16 +12,16 @@ type GetBlockHeightConfig struct {
 }
 
 // GetBlockHeight returns the current block height of the node
-func (c *RpcClient) GetBlockHeight(ctx context.Context) (GetBlockHeightResponse, error) {
+func (c *RpcClient) GetBlockHeight(ctx context.Context) (JsonRpcResponse[uint64], error) {
 	return c.processGetBlockHeight(c.Call(ctx, "getBlockHeight"))
 }
 
 // GetBlockHeightWithConfig returns the current block height of the node
-func (c *RpcClient) GetBlockHeightWithConfig(ctx context.Context, cfg GetBlockHeightConfig) (GetBlockHeightResponse, error) {
+func (c *RpcClient) GetBlockHeightWithConfig(ctx context.Context, cfg GetBlockHeightConfig) (JsonRpcResponse[uint64], error) {
 	return c.processGetBlockHeight(c.Call(ctx, "getBlockHeight", cfg))
 }
 
-func (c *RpcClient) processGetBlockHeight(body []byte, rpcErr error) (res GetBlockHeightResponse, err error) {
+func (c *RpcClient) processGetBlockHeight(body []byte, rpcErr error) (res JsonRpcResponse[uint64], err error) {
 	err = c.processRpcCall(body, rpcErr, &res)
 	return
 }
--- rpc/get_block_height_test.go
+++ rpc/get_block_height_test.go
@@ -13,13 +13,11 @@ func TestGetBlockHeight(t *testing.T) {
 			RpcCall: func(rc RpcClient) (any, error) {
 				return rc.GetBlockHeight(context.TODO())
 			},
-			ExpectedResponse: GetBlockHeightResponse{
-				GeneralResponse: GeneralResponse{
-					JsonRPC: "2.0",
-					ID:      1,
-					Error:   nil,
-				},
-				Result: 83518197,
+			ExpectedResponse: JsonRpcResponse[uint64]{
+				JsonRpc: "2.0",
+				Id:      1,
+				Error:   nil,
+				Result:  83518197,
 			},
 			ExpectedError: nil,
 		},
@@ -34,13 +32,11 @@ func TestGetBlockHeight(t *testing.T) {
 					},
 				)
 			},
-			ExpectedResponse: GetBlockHeightResponse{
-				GeneralResponse: GeneralResponse{
-					JsonRPC: "2.0",
-					ID:      1,
-					Error:   nil,
-				},
-				Result: 83518231,
+			ExpectedResponse: JsonRpcResponse[uint64]{
+				JsonRpc: "2.0",
+				Id:      1,
+				Error:   nil,
+				Result:  83518231,
 			},
 			ExpectedError: nil,
 		},
