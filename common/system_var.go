652451bfb041780f6a1900b4b8ad2e3bd45a68d7
Yihau Chen <a122092487@gmail.com>
2022-09-26 02:58:40 +0800

    add slot hashes pubkey

--- common/system_var.go
+++ common/system_var.go
@@ -8,5 +8,6 @@ var (
 	SysVarRewardsPubkey          = PublicKeyFromString("SysvarRewards111111111111111111111111111111")
 	SysVarStakeHistoryPubkey     = PublicKeyFromString("SysvarStakeHistory1111111111111111111111111")
 	SysVarInstructionsPubkey     = PublicKeyFromString("Sysvar1nstructions1111111111111111111111111")
+	SysVarSlotHashesPubkey       = PublicKeyFromString("SysvarS1otHashes111111111111111111111111111")
 	StakeConfigPubkey            = PublicKeyFromString("StakeConfig11111111111111111111111111111111")
 )
