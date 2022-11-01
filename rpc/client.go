06580a7abbd3335b3bbfd0e5d4422dfcaaf1e085
Yihau Chen <a122092487@gmail.com>
2022-11-02 03:02:16 +0800

    feat: add private generic call method

--- rpc/client.go
+++ rpc/client.go
@@ -128,3 +128,21 @@ func (c *RpcClient) processRpcCall(body []byte, rpcErr error, res any) error {
 	}
 	return nil
 }
+
+func call[T any](c *RpcClient, ctx context.Context, params ...any) (T, error) {
+	var output T
+
+	// rpc call
+	body, err := c.Call(ctx, params...)
+	if err != nil {
+		return output, fmt.Errorf("rpc: call error, err: %v, body: %v", err, string(body))
+	}
+
+	// transfer data
+	err = json.Unmarshal(body, &output)
+	if err != nil {
+		return output, fmt.Errorf("rpc: failed to json decode body, err: %v", err)
+	}
+
+	return output, nil
+}
