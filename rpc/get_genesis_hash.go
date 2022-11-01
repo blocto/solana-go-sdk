323beec26c6fceba97ba9f14fdbf04f2acce1714
Yihau Chen <a122092487@gmail.com>
2022-11-02 03:22:42 +0800

    chore: use the private call method in rpc/get_genesis_hash.go

--- rpc/get_genesis_hash.go
+++ rpc/get_genesis_hash.go
@@ -8,10 +8,5 @@ type GetGenesisHashResponse JsonRpcResponse[string]
 
 // GetGenesisHash returns the genesis hash
 func (c *RpcClient) GetGenesisHash(ctx context.Context) (JsonRpcResponse[string], error) {
-	return c.processGetGenesisHash(c.Call(ctx, "getGenesisHash"))
-}
-
-func (c *RpcClient) processGetGenesisHash(body []byte, rpcErr error) (res JsonRpcResponse[string], err error) {
-	err = c.processRpcCall(body, rpcErr, &res)
-	return
+	return call[JsonRpcResponse[string]](c, ctx, "getGenesisHash")
 }
