2a1e61ad94dcac47933899e959aa8857bfd8ddaf
Yihau Chen <a122092487@gmail.com>
2022-11-02 03:21:36 +0800

    chore: use the private call method in rpc/get_blocks_with_limit.go

--- rpc/get_blocks_with_limit.go
+++ rpc/get_blocks_with_limit.go
@@ -11,15 +11,10 @@ type GetBlocksWithLimitConfig struct {
 
 // GetBlocksWithLimit eturns a list of confirmed blocks starting at the given slot
 func (c *RpcClient) GetBlocksWithLimit(ctx context.Context, startSlot uint64, limit uint64) (JsonRpcResponse[[]uint64], error) {
-	return c.processGetBlocksWithLimit(c.Call(ctx, "getBlocksWithLimit", startSlot, limit))
+	return call[JsonRpcResponse[[]uint64]](c, ctx, "getBlocksWithLimit", startSlot, limit)
 }
 
 // GetBlocksWithLimit eturns a list of confirmed blocks starting at the given slot
 func (c *RpcClient) GetBlocksWithLimitWithConfig(ctx context.Context, startSlot uint64, limit uint64, cfg GetBlocksWithLimitConfig) (JsonRpcResponse[[]uint64], error) {
-	return c.processGetBlocksWithLimit(c.Call(ctx, "getBlocksWithLimit", startSlot, limit, cfg))
-}
-
-func (c *RpcClient) processGetBlocksWithLimit(body []byte, rpcErr error) (res JsonRpcResponse[[]uint64], err error) {
-	err = c.processRpcCall(body, rpcErr, &res)
-	return
+	return call[JsonRpcResponse[[]uint64]](c, ctx, "getBlocksWithLimit", startSlot, limit, cfg)
 }
