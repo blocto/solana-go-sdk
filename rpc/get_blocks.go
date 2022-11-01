be7682b1e7f5e1d4469030f864e0acec709eeafa
Yihau Chen <a122092487@gmail.com>
2022-11-02 03:21:28 +0800

    chore: use the private call method in rpc/get_blocks.go

--- rpc/get_blocks.go
+++ rpc/get_blocks.go
@@ -12,16 +12,11 @@ type GetBlocksConfig struct {
 // GetBlocks returns a list of confirmed blocks between two slots
 // Max range allowed is 500,000 slot
 func (c *RpcClient) GetBlocks(ctx context.Context, startSlot uint64, endSlot uint64) (JsonRpcResponse[[]uint64], error) {
-	return c.processGetBlocks(c.Call(ctx, "getBlocks", startSlot, endSlot))
+	return call[JsonRpcResponse[[]uint64]](c, ctx, "getBlocks", startSlot, endSlot)
 }
 
 // GetBlocks returns a list of confirmed blocks between two slots
 // Max range allowed is 500,000 slot
 func (c *RpcClient) GetBlocksWithConfig(ctx context.Context, startSlot uint64, endSlot uint64, cfg GetBlocksConfig) (JsonRpcResponse[[]uint64], error) {
-	return c.processGetBlocks(c.Call(ctx, "getBlocks", startSlot, endSlot, cfg))
-}
-
-func (c *RpcClient) processGetBlocks(body []byte, rpcErr error) (res JsonRpcResponse[[]uint64], err error) {
-	err = c.processRpcCall(body, rpcErr, &res)
-	return
+	return call[JsonRpcResponse[[]uint64]](c, ctx, "getBlocks", startSlot, endSlot, cfg)
 }
