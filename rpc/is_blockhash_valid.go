d9a5f70ae25da652d4df8a29404277f80e7bfe72
Yihau Chen <a122092487@gmail.com>
2022-11-02 03:24:25 +0800

    chore: use the private call method in rpc/is_blockhash_valid.go

--- rpc/is_blockhash_valid.go
+++ rpc/is_blockhash_valid.go
@@ -20,16 +20,11 @@ type IsBlockhashValidConfig struct {
 // NEW: This method is only available in solana-core v1.9 or newer. Please use getFees for solana-core v1.8
 // IsBlockhashValid get the fee the network will charge for a particular Message
 func (c *RpcClient) IsBlockhashValid(ctx context.Context, message string) (JsonRpcResponse[IsBlockhashValid], error) {
-	return c.processIsBlockhashValid(c.Call(ctx, "isBlockhashValid", message))
+	return call[JsonRpcResponse[IsBlockhashValid]](c, ctx, "isBlockhashValid", message)
 }
 
 // NEW: This method is only available in solana-core v1.9 or newer. Please use getFees for solana-core v1.8
 // IsBlockhashValidWithConfig get the fee the network will charge for a particular Message
 func (c *RpcClient) IsBlockhashValidWithConfig(ctx context.Context, message string, cfg IsBlockhashValidConfig) (JsonRpcResponse[IsBlockhashValid], error) {
-	return c.processIsBlockhashValid(c.Call(ctx, "isBlockhashValid", message, cfg))
-}
-
-func (c *RpcClient) processIsBlockhashValid(body []byte, rpcErr error) (res JsonRpcResponse[IsBlockhashValid], err error) {
-	err = c.processRpcCall(body, rpcErr, &res)
-	return
+	return call[JsonRpcResponse[IsBlockhashValid]](c, ctx, "isBlockhashValid", message, cfg)
 }
