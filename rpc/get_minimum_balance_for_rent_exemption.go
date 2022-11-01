8ad82fee7c91a424de5f09f9f4b72c9fb49b9a99
Yihau Chen <a122092487@gmail.com>
2022-11-02 03:23:18 +0800

    chore: use the private call method in rpc/get_minimum_balance_for_rent_exemption.go

--- rpc/get_minimum_balance_for_rent_exemption.go
+++ rpc/get_minimum_balance_for_rent_exemption.go
@@ -12,15 +12,10 @@ type GetMinimumBalanceForRentExemptionConfig struct {
 
 // GetMinimumBalanceForRentExemption returns minimum balance required to make account rent exempt
 func (c *RpcClient) GetMinimumBalanceForRentExemption(ctx context.Context, dataLen uint64) (JsonRpcResponse[uint64], error) {
-	return c.processGetMinimumBalanceForRentExemption(c.Call(ctx, "getMinimumBalanceForRentExemption", dataLen))
+	return call[JsonRpcResponse[uint64]](c, ctx, "getMinimumBalanceForRentExemption", dataLen)
 }
 
 // GetMinimumBalanceForRentExemptionWithConfig returns minimum balance required to make account rent exempt
 func (c *RpcClient) GetMinimumBalanceForRentExemptionWithConfig(ctx context.Context, dataLen uint64, cfg GetMinimumBalanceForRentExemptionConfig) (JsonRpcResponse[uint64], error) {
-	return c.processGetMinimumBalanceForRentExemption(c.Call(ctx, "getMinimumBalanceForRentExemption", dataLen, cfg))
-}
-
-func (c *RpcClient) processGetMinimumBalanceForRentExemption(body []byte, rpcErr error) (res JsonRpcResponse[uint64], err error) {
-	err = c.processRpcCall(body, rpcErr, &res)
-	return
+	return call[JsonRpcResponse[uint64]](c, ctx, "getMinimumBalanceForRentExemption", dataLen, cfg)
 }
