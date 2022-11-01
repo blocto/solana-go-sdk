f4f10696dfa7e74dcba02208d5f359a3c338b4f0
Yihau Chen <a122092487@gmail.com>
2022-11-02 03:21:12 +0800

    chore: use the private call method in rpc/get_block_time.go

--- rpc/get_block_time.go
+++ rpc/get_block_time.go
@@ -8,10 +8,5 @@ type GetBlockTimeResponse JsonRpcResponse[int64]
 
 // GetBlockTime returns the estimated production time of a block.
 func (c *RpcClient) GetBlockTime(ctx context.Context, slot uint64) (JsonRpcResponse[int64], error) {
-	return c.processGetBlockTime(c.Call(ctx, "getBlockTime", slot))
-}
-
-func (c *RpcClient) processGetBlockTime(body []byte, rpcErr error) (res JsonRpcResponse[int64], err error) {
-	err = c.processRpcCall(body, rpcErr, &res)
-	return
+	return call[JsonRpcResponse[int64]](c, ctx, "getBlockTime", slot)
 }
