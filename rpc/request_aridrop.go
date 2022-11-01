96d6c4f93faa1fcc880b1c3be58caebab9367b4e
Yihau Chen <a122092487@gmail.com>
2022-11-02 03:24:31 +0800

    chore: use the private call method in rpc/request_aridrop.go

--- rpc/request_aridrop.go
+++ rpc/request_aridrop.go
@@ -13,15 +13,10 @@ type RequestAirdropConfig struct {
 
 // RequestAirdrop requests an airdrop of lamports to a Pubkey
 func (c *RpcClient) RequestAirdrop(ctx context.Context, base58Addr string, lamports uint64) (JsonRpcResponse[string], error) {
-	return c.processRequestAirdrop(c.Call(ctx, "requestAirdrop", base58Addr, lamports))
+	return call[JsonRpcResponse[string]](c, ctx, "requestAirdrop", base58Addr, lamports)
 }
 
 // RequestAirdropWithConfig requests an airdrop of lamports to a Pubkey
 func (c *RpcClient) RequestAirdropWithConfig(ctx context.Context, base58Addr string, lamports uint64, cfg RequestAirdropConfig) (JsonRpcResponse[string], error) {
-	return c.processRequestAirdrop(c.Call(ctx, "requestAirdrop", base58Addr, lamports, cfg))
-}
-
-func (c *RpcClient) processRequestAirdrop(body []byte, rpcErr error) (res JsonRpcResponse[string], err error) {
-	err = c.processRpcCall(body, rpcErr, &res)
-	return
+	return call[JsonRpcResponse[string]](c, ctx, "requestAirdrop", base58Addr, lamports, cfg)
 }
