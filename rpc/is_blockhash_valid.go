27c3c5dca4e47125d909832e7f1ddc49b916b177
yihau <yihau.chen@icloud.com>
2023-01-09 00:54:47 +0800

    refactor(client): is blockhash valid

--- rpc/is_blockhash_valid.go
+++ rpc/is_blockhash_valid.go
@@ -13,14 +13,12 @@ type IsBlockhashValidConfig struct {
 	Commitment Commitment `json:"commitment,omitempty"`
 }
 
-// NEW: This method is only available in solana-core v1.9 or newer. Please use getFees for solana-core v1.8
 // IsBlockhashValid get the fee the network will charge for a particular Message
-func (c *RpcClient) IsBlockhashValid(ctx context.Context, message string) (JsonRpcResponse[IsBlockhashValid], error) {
-	return call[JsonRpcResponse[IsBlockhashValid]](c, ctx, "isBlockhashValid", message)
+func (c *RpcClient) IsBlockhashValid(ctx context.Context, message string) (JsonRpcResponse[ValueWithContext[bool]], error) {
+	return call[JsonRpcResponse[ValueWithContext[bool]]](c, ctx, "isBlockhashValid", message)
 }
 
-// NEW: This method is only available in solana-core v1.9 or newer. Please use getFees for solana-core v1.8
 // IsBlockhashValidWithConfig get the fee the network will charge for a particular Message
-func (c *RpcClient) IsBlockhashValidWithConfig(ctx context.Context, message string, cfg IsBlockhashValidConfig) (JsonRpcResponse[IsBlockhashValid], error) {
-	return call[JsonRpcResponse[IsBlockhashValid]](c, ctx, "isBlockhashValid", message, cfg)
+func (c *RpcClient) IsBlockhashValidWithConfig(ctx context.Context, message string, cfg IsBlockhashValidConfig) (JsonRpcResponse[ValueWithContext[bool]], error) {
+	return call[JsonRpcResponse[ValueWithContext[bool]]](c, ctx, "isBlockhashValid", message, cfg)
 }
