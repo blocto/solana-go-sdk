8b3fb36bb4ae9069d7decf61b57512b7a1afd45b
yihau <yihau.chen@icloud.com>
2023-01-10 03:40:14 +0800

    fix(rpc): get block time may be null

--- rpc/get_block_time.go
+++ rpc/get_block_time.go
@@ -4,9 +4,9 @@ import (
 	"context"
 )
 
-type GetBlockTimeResponse JsonRpcResponse[int64]
+type GetBlockTimeResponse JsonRpcResponse[*int64]
 
 // GetBlockTime returns the estimated production time of a block.
-func (c *RpcClient) GetBlockTime(ctx context.Context, slot uint64) (JsonRpcResponse[int64], error) {
-	return call[JsonRpcResponse[int64]](c, ctx, "getBlockTime", slot)
+func (c *RpcClient) GetBlockTime(ctx context.Context, slot uint64) (JsonRpcResponse[*int64], error) {
+	return call[JsonRpcResponse[*int64]](c, ctx, "getBlockTime", slot)
 }
--- rpc/get_block_time_test.go
+++ rpc/get_block_time_test.go
@@ -5,9 +5,11 @@ import (
 	"testing"
 
 	"github.com/portto/solana-go-sdk/internal/client_test"
+	"github.com/portto/solana-go-sdk/pkg/pointer"
 )
 
 func TestGetBlockTime(t *testing.T) {
+	var nilValue *int64
 	client_test.TestAll(
 		t,
 		[]client_test.Param{
@@ -21,14 +23,14 @@ func TestGetBlockTime(t *testing.T) {
 						100000,
 					)
 				},
-				ExpectedValue: JsonRpcResponse[int64]{
+				ExpectedValue: JsonRpcResponse[*int64]{
 					JsonRpc: "2.0",
 					Id:      1,
 					Error: &JsonRpcError{
 						Code:    -32009,
 						Message: "Slot 100000 was skipped, or missing in long-term storage",
 					},
-					Result: 0,
+					Result: nilValue,
 				},
 				ExpectedError: nil,
 			},
@@ -42,14 +44,14 @@ func TestGetBlockTime(t *testing.T) {
 						100048426,
 					)
 				},
-				ExpectedValue: JsonRpcResponse[int64]{
+				ExpectedValue: JsonRpcResponse[*int64]{
 					JsonRpc: "2.0",
 					Id:      1,
 					Error: &JsonRpcError{
 						Code:    -32004,
 						Message: "Block not available for slot 100048426",
 					},
-					Result: 0,
+					Result: nilValue,
 				},
 				ExpectedError: nil,
 			},
@@ -63,11 +65,29 @@ func TestGetBlockTime(t *testing.T) {
 						85588104,
 					)
 				},
-				ExpectedValue: JsonRpcResponse[int64]{
+				ExpectedValue: JsonRpcResponse[*int64]{
 					JsonRpc: "2.0",
 					Id:      1,
 					Error:   nil,
-					Result:  1633531934,
+					Result:  pointer.Get[int64](1633531934),
+				},
+				ExpectedError: nil,
+			},
+			{
+				RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getBlockTime", "params":[85588104]}`,
+				ResponseBody: `{"jsonrpc":"2.0","result":null,"id":1}`,
+				F: func(url string) (any, error) {
+					c := NewRpcClient(url)
+					return c.GetBlockTime(
+						context.TODO(),
+						85588104,
+					)
+				},
+				ExpectedValue: JsonRpcResponse[*int64]{
+					JsonRpc: "2.0",
+					Id:      1,
+					Error:   nil,
+					Result:  nilValue,
 				},
 				ExpectedError: nil,
 			},
