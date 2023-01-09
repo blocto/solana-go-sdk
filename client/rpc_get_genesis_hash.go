337aa99e4bbfa52c68da98788f140caff729b591
yihau <yihau.chen@icloud.com>
2023-01-10 03:15:38 +0800

    refactor(client): get genesis hash

---
+++ client/rpc_get_genesis_hash.go
@@ -0,0 +1,17 @@
+package client
+
+import (
+	"context"
+
+	"github.com/portto/solana-go-sdk/rpc"
+)
+
+// GetGenesisHash returns the genesis hash
+func (c *Client) GetGenesisHash(ctx context.Context) (string, error) {
+	return process(
+		func() (rpc.JsonRpcResponse[string], error) {
+			return c.RpcClient.GetGenesisHash(ctx)
+		},
+		forward[string],
+	)
+}
