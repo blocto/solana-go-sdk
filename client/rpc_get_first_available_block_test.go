5b10c710282aaa6bf524296debf5ada3796f6122
yihau <yihau.chen@icloud.com>
2023-01-10 03:13:16 +0800

    refactor(client): get first available block

---
+++ client/rpc_get_first_available_block_test.go
@@ -0,0 +1,28 @@
+package client
+
+import (
+	"context"
+	"testing"
+
+	"github.com/portto/solana-go-sdk/internal/client_test"
+)
+
+func TestClient_GetFirstAvailableBlock(t *testing.T) {
+	client_test.TestAll(
+		t,
+		[]client_test.Param{
+			{
+				RequestBody:  `{"jsonrpc":"2.0", "id":1, "method":"getFirstAvailableBlock"}`,
+				ResponseBody: `{"jsonrpc":"2.0","result":100,"id":1}`,
+				F: func(url string) (any, error) {
+					c := NewClient(url)
+					return c.GetFirstAvailableBlock(
+						context.TODO(),
+					)
+				},
+				ExpectedValue: uint64(100),
+				ExpectedError: nil,
+			},
+		},
+	)
+}
