2279865cbd8e279475f90dbf3f4df41dc29c851c
yihau <yihau.chen@icloud.com>
2023-01-10 03:43:03 +0800

    refactor(client): get block time

---
+++ client/rpc_get_block_time.go
@@ -0,0 +1,17 @@
+package client
+
+import (
+	"context"
+
+	"github.com/portto/solana-go-sdk/rpc"
+)
+
+// GetBlockTime returns the estimated production time of a block.
+func (c *Client) GetBlockTime(ctx context.Context, slot uint64) (*int64, error) {
+	return process(
+		func() (rpc.JsonRpcResponse[*int64], error) {
+			return c.RpcClient.GetBlockTime(ctx, slot)
+		},
+		forward[*int64],
+	)
+}
