fc37a88c87a5bde753a586b89a689f7be66ae91e
yihau <yihau.chen@icloud.com>
2023-01-10 03:20:09 +0800

    refactor(client): get version

---
+++ client/rpc_get_version.go
@@ -0,0 +1,17 @@
+package client
+
+import (
+	"context"
+
+	"github.com/portto/solana-go-sdk/rpc"
+)
+
+// GetVersion returns the current solana versions running on the node
+func (c *Client) GetVersion(ctx context.Context) (rpc.GetVersion, error) {
+	return process(
+		func() (rpc.JsonRpcResponse[rpc.GetVersion], error) {
+			return c.RpcClient.GetVersion(ctx)
+		},
+		forward[rpc.GetVersion],
+	)
+}
