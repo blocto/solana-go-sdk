d9e4244ec3452c0cf08a2aca5475312fe30e321d
yihau <yihau.chen@icloud.com>
2023-03-04 15:07:16 +0800

    feat: add CreateIdempotent

--- program/associated_token_account/instruction.go
+++ program/associated_token_account/instruction.go
@@ -47,6 +47,39 @@ func CreateAssociatedTokenAccount(param CreateAssociatedTokenAccountParam) types
 	}
 }
 
+type CreateIdempotentParam struct {
+	Funder                 common.PublicKey
+	Owner                  common.PublicKey
+	Mint                   common.PublicKey
+	AssociatedTokenAccount common.PublicKey
+}
+
+// CreateIdempotent is the only instruction in associated token program
+func CreateIdempotent(param CreateIdempotentParam) types.Instruction {
+	data, err := borsh.Serialize(struct {
+		Instruction Instruction
+	}{
+		Instruction: InstructionCreateIdempotent,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return types.Instruction{
+		ProgramID: common.SPLAssociatedTokenAccountProgramID,
+		Accounts: []types.AccountMeta{
+			{PubKey: param.Funder, IsSigner: true, IsWritable: true},
+			{PubKey: param.AssociatedTokenAccount, IsSigner: false, IsWritable: true},
+			{PubKey: param.Owner, IsSigner: false, IsWritable: false},
+			{PubKey: param.Mint, IsSigner: false, IsWritable: false},
+			{PubKey: common.SystemProgramID, IsSigner: false, IsWritable: false},
+			{PubKey: common.TokenProgramID, IsSigner: false, IsWritable: false},
+			{PubKey: common.SysVarRentPubkey, IsSigner: false, IsWritable: false},
+		},
+		Data: data,
+	}
+}
+
 type RecoverNestedParam struct {
 	Owner                             common.PublicKey
 	OwnerMint                         common.PublicKey
